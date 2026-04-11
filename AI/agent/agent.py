208026fc159675553eec6bf73d483fb52d39b0c7
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-04-11 20:43:34 +0500

    The agent converted to async

--- AI/agent/agent.py
+++ AI/agent/agent.py
@@ -1,10 +1,8 @@
 from agents import Agent, Runner, RunContextWrapper
 from dotenv import load_dotenv
 from user_data.user_data import UserData
+from typing import Dict
 load_dotenv()
-
-
-prev_history_id: str = None
 
 def instruction_generator(wrapper: RunContextWrapper[UserData], agent: Agent) -> str:
     """Generates instructions for the agent based on the user data context."""
@@ -12,28 +10,24 @@ def instruction_generator(wrapper: RunContextWrapper[UserData], agent: Agent) ->
     return f"You are a helpful assistant. Your name is {agent.name} The user is {context.name} from {context.location}. His role is {context.role}. They have the following tags: {', '.join(context.tags)}. Answer the user's question based on this information."
 
 
+async def agent_caller(user_input: str, context: UserData, prev_response_id: str) -> Dict[str, str]:
+    """Calls the agent and returns the response and the history ID."""
 
-
-chat_agent = Agent(
-    name="ChatAgent",
-    instructions=instruction_generator,
-    model="gpt-3.5-turbo"
-)
-
-while True:
-    user_input = input("User: ")
-    response = Runner.run_sync(
-        starting_agent=chat_agent,
-        input=user_input,
-        previous_response_id=prev_history_id,
-        context=UserData(
-            _id="123",
-            name="John Doe",
-            email="123@gmail.com",
-            location="New York",
-            tags=["Programmer", "AI Enthusiast"],
-            role="sponsor"
-        ),
+    chat_agent = Agent(
+        name="ChatAgent",
+        instructions=instruction_generator,
+        model="gpt-3.5-turbo"
     )
-    print(f"ChatAgent: {response.final_output}")
-    prev_history_id = response.last_response_id
+
+    while True:
+        response = await Runner.run(
+            starting_agent=chat_agent,
+            input=user_input,
+            previous_response_id=prev_response_id,
+            context=context
+        )
+
+        return {
+            "response": response.final_output,
+            "history_id": response.last_response_id
+        }
