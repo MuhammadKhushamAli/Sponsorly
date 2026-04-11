de7feb703c5f47b5871e6cde32a6d8e836f30c7f
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-04-11 20:32:23 +0500

    Data clssand aent added

---
+++ AI/agent/agent.py
@@ -0,0 +1,38 @@
+from agents import Agent, Runner, function_tool, RunContextWrapper
+from dotenv import load_dotenv
+from user_data.user_data import UserData
+load_dotenv()
+
+
+prev_history_id: str = None
+
+def instruction_generator(wrapper: RunContextWrapper[UserData], agent: Agent) -> str:
+    context: UserData = wrapper.context
+    return f"You are a helpful assistant. Your name is {agent.name} The user is {context.name} from {context.location}. His role is {context.role}. They have the following tags: {', '.join(context.tags)}. Answer the user's question based on this information."
+
+
+
+
+chat_agent = Agent(
+    name="ChatAgent",
+    instructions=instruction_generator,
+    model="gpt-3.5-turbo"
+)
+
+while True:
+    user_input = input("User: ")
+    response = Runner.run_sync(
+        starting_agent=chat_agent,
+        input=user_input,
+        previous_response_id=prev_history_id,
+        context=UserData(
+            _id="123",
+            name="John Doe",
+            email="123@gmail.com",
+            location="New York",
+            tags=["Programmer", "AI Enthusiast"],
+            role="sponsor"
+        ),
+    )
+    print(f"ChatAgent: {response.final_output}")
+    prev_history_id = response.last_response_id
---
+++ AI/user_data/user_data.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import Literal, List
+
+@dataclass
+class UserData:
+    _id: str
+    name: str
+    email: str
+    location: str
+    tags: list[str]
+    role: Literal["sponsor", "creator"]
+
+    def __post_init__(self):
+        allowed_roles: List[str] = ["sponsor", "creator"]
+
+        if self.role not in allowed_roles:
+            raise ValueError(f"Invalid role: {self.role}. Allowed roles are: {', '.join(allowed_roles)}")
