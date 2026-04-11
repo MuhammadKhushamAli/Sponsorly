7876188ea51d0513d64caba08ad000f2e1947531
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-04-11 21:17:16 +0500

    Connect Database

--- AI/db/connect_db.py
+++ AI/db/connect_db.py
@@ -1,1 +1,26 @@
 from dotenv import load_dotenv
+from pymongo import AsyncMongoClient
+import pymongo
+import os
+
+async def connect_db() -> AsyncMongoClient:
+    """Connects to the MongoDB database asyncrounously and return the client"""
+    load_dotenv()
+    MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+    
+    if not MONGO_DB_URI:
+        raise ValueError("MONGO_DB_URI is not set in the environment variables.")
+    
+    try:
+        mongo_db_client: AsyncMongoClient = AsyncMongoClient(
+            MONGO_DB_URI,
+            server_api=pymongo.server_api.ServerApi(
+                version="1",
+                strict=True,
+                deprecation_errors=True
+            )
+        )
+        return mongo_db_client
+    except Exception as e:
+        raise Exception("Unable to Connect to Mongo DB")
+    
