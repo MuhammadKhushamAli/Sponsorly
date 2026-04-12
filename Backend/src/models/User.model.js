2c49864eac52a6a267110768f7aa2cff3f2d04a2
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-04-12 22:16:25 +0500

    User Model Updated

--- Backend/src/models/User.model.js
+++ Backend/src/models/User.model.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, "Password is required"],
     },
+    prevChatBotHistoryID: {
+      type: String,
+      default: null
+    },
     refreshToken: {
       type: String,
     },
