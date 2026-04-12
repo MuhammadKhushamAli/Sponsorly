649d5097325c8642da86c57a1dba71977f2e0faa
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-04-12 22:03:31 +0500

    Passing data errors fixed

--- AI/fast_api_server/server.py
+++ AI/fast_api_server/server.py
@@ -3,6 +3,7 @@ from .pydantic_model.chat_bot_message_model import ChatBotMessage
 from .pydantic_model.user_context_model import UserContext
 from agent.agent import agent_caller
 from typing import Dict
+from user_data.user_data import UserData
 
 app: FastAPI = FastAPI()
 
@@ -21,9 +22,16 @@ async def interacting_with_agent(
     message: ChatBotMessage = Body(..., description="The user query for the chat bot to start the conversation"),
     context: UserContext = Body(..., description="It is required to setup the context of user for chat bot")
 ):
+
     chat_bot_reply: Dict[str, str] = await agent_caller(
-        user_input=message,
-        context=context,
+        user_input=message.message,
+        context=UserData(
+            name=context.user_name,
+            email=context.user_email,
+            location=context.user_location,
+            tags=context.user_tage,
+            role=context.user_role
+        ),
         prev_response_id=prev_history_id
     )
 
