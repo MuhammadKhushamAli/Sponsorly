eb3340e1fef1908ff52b09f0fa72944bc59a1419
Shaiq Raza <shaiqraza808y@gmail.com>
2026-04-29 16:58:51 +0500

    update creator model/Backend

--- Backend/src/models/Creator.model.js
+++ Backend/src/models/Creator.model.js
@@ -7,19 +7,65 @@ const creatorSchema = new mongoose.Schema(
       ref: "User",
       required: [true, "User reference is required"],
     },
+
     previousProjects: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Project",
       },
     ],
+
     ratings: [
       {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Review",
+        review: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Review",
+        },
+        rating: {
+          type: Number,
+          min: 1,
+          max: 5,
+          required: true,
+        },
       },
     ],
+
     niche: [{ type: String }],
+
+    bio: {
+      type: String,
+      trim: true,
+    },
+
+    links: {
+      type: [
+        {
+          platform: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+          url: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+        },
+      ],
+      validate: {
+        validator: function (value) {
+          return value.length > 0;
+        },
+        message: "At least one social platform link is required",
+      },
+      required: true,
+    },
+
+    followersCount: {
+      type: Number,
+      required: [true, "Followers count is required"],
+      min: 0,
+    },
   },
   {
     timestamps: true,
