950c2934abc066b63b2502d58b7bbb9d9266ece0
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-05-12 07:36:27 +0500

    chat end point added

--- Backend/src/routes/chat.routes.js
+++ Backend/src/routes/chat.routes.js
@@ -1,11 +1,20 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
-import { createDirectChat, addMessageToDirectChat, addMessageToProjectChat } from "../controllers/chat.controller.js";
+import {
+  getMyChats,
+  getChatById,
+  createDirectChat,
+  addMessageToDirectChat,
+  addMessageToProjectChat,
+} from "../controllers/chat.controller.js";
 
 const router = express.Router();
 
+router.get("/my-chats", verifyToken, getMyChats);
+router.get("/:chatId", verifyToken, getChatById);
 router.post("/create/:otherUserId", verifyToken, createDirectChat);
 router.post("/:chatId/message", verifyToken, addMessageToDirectChat);
 router.post("/project/:projectId/message", verifyToken, addMessageToProjectChat);
 
 export default router;
+
